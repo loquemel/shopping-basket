409caee9dc58b86123ad6153cad13ee15d59cca7
Melvin Loquero <loqueromelvin@gmail.com>
2019-01-26 15:24:29 +0800

    Initial commmit
    Signed-off-by: Melvin Loquero <loqueromelvin@gmail.com>

---
+++ src/main/java/com/shopping/basket/ShoppingBasketApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBasketApplication.class, args);
+	}
+
+}
+
